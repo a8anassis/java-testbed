93365e9d35ae4fdf2a980961332280500a39f323
a8anassis <a8anassis@gmail.com>
2020-10-06 11:49:13 +0300

    Hello World v 0.2

--- src/gr/aueb/elearn/ch1/HelloWorld.java
+++ src/gr/aueb/elearn/ch1/HelloWorld.java
@@ -1,5 +1,9 @@
 package gr.aueb.elearn.ch1;
 
+/**
+ * Prints Hello World
+ * @author Th. Androutsos   
+ */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
