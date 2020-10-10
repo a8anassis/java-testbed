3b5d02cc80cbcd7bae87708b5692ee91a6c0f3fc
a8anassis <a8anassis@gmail.com>
2020-10-10 10:44:27 +0300

    Add Demo v.0.2

---
+++ src/gr/aueb/elearn/ch2/Add2Demo.java
@@ -0,0 +1,13 @@
+package gr.aueb.elearn.ch2;
+
+public class Add2Demo {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int result;
+
+        result = a + b;
+
+        System.out.printf("The result of %d + %d is %d", a, b, result);
+    }
+}
